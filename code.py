bce19e8ab83ed36357b8cf14aa099b1c27029f9d
Michelangelo Foschi <michelangelo.foschi.10@gmail.com>
2020-12-07 17:46:08 +0400

    add code

--- code.py
+++ code.py
@@ -1,1 +1,14 @@
+from gtts import gTTS 
 
+import os 
+
+mytext = '#Add your text here'
+
+language = 'en'
+
+myobj = gTTS(text=mytext, lang=language, slow=False) 
+
+myobj.save("welcome.mp3") 
+  
+
+os.system("mpg321 welcome.mp3") 
